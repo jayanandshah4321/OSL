f469ce27bc2b83376be435f18349840db885a9b0
jayanandshah4321 <156643732+jayanandshah4321@users.noreply.github.com>
2024-10-29 13:03:57 +0530

    Create ass2bchild.c

---
+++ ass2bchild.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Array in reverse order:\n");
+
+    for (int i = argc - 1; i > 0; i--) {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
